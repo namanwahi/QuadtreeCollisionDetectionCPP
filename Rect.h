1ad9d4ea97ed05726119732f98f1a6fd7865a620
nw316 <nw316@ic.ac.uk>
2017-08-21 14:58:31 +0100

    added Rect class and implementation

---
+++ Rect.h
@@ -0,0 +1,16 @@
+#ifndef RECT_H
+#define RECT_H
+
+struct Rect {
+	//x and y position of top left corner
+	int x;
+	int y;
+
+	//width and height of the rectangle
+	int w;
+	int h;
+
+	bool intersect(const Rect&) const;
+}
+
+#endif
